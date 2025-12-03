1999fad1dee714f5ce01bc721e825ee1f2be766c
Dolliving <treasure_tree@qq.com>
2025-12-03 23:17:29 +0800

    firstFile更改BMI计算

--- firstFile.py
+++ firstFile.py
@@ -1,1 +1,8 @@
 print("Dad!!")
+# BMI计算 BMI = 体重 / （身高 ** 2）
+user_weight = float(input("请输入你的体重（单位：kg）："))
+user_height = float(input("请输入你的身高（单位：m）："))
+
+user_BMI = user_weight / (user_height ** 2)
+
+print("你的BMI为：" + str(user_BMI))
